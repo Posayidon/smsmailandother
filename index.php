fe5c3d27b2c0daaacacb488a747ae9b2cc5afd32
Thomas <thomaslepretre78@gmail.com>
2021-01-10 15:11:43 +0100

    retirer les balises

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ if ($to == 'thomaslepretre78@gmail.com' || $to == 'charlescretois@gmail.com'){
   http_response_code(201);
   echo("success\n");
 }else{
-  http_response_code(201)
+  http_response_code(201);
 }
 
 echo("Envelope:\n");
