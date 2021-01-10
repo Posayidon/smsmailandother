f80c7291720c0972b7643c0f7e7fd998af940214
Thomas <thomaslepretre78@gmail.com>
2021-01-10 15:09:06 +0100

    retirer les balises

--- index.php
+++ index.php
@@ -6,8 +6,7 @@ if ($to == 'thomaslepretre78@gmail.com' || $to == 'charlescretois@gmail.com'){
   http_response_code(201);
   echo("success\n");
 }else{
-  http_response_code(403);
-  echo("user not allowed here\n");
+  http_response_code(201)
 }
 
 echo("Envelope:\n");
