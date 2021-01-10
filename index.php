4d0abc2b40deeb9a9e3ef0b6f0d754b531197725
Thomas <thomaslepretre78@gmail.com>
2021-01-10 14:14:05 +0100

    test mail

--- index.php
+++ index.php
@@ -5,6 +5,32 @@
     <title></title>
   </head>
   <body>
-    ca marche
+<?php
+header("Content-type: text/plain");
+
+$to = $_POST['envelope']['to'];
+if ($to == 'allowed@example.com'){
+  http_response_code(201);
+  echo("success\n");
+}else{
+  http_response_code(403);
+  echo("user not allowed here\n");
+}
+
+echo("Envelope:\n");
+print_r($_POST['envelope']);
+
+echo("Headers:\n");
+print_r($_POST['headers']);
+
+echo("Plain:\n");
+echo($_POST['plain']."\n");
+
+echo("HTML:\n");
+echo($_POST['html']."\n");
+
+echo("Attachments:\n");
+print_r($_FILES["attachments"]);
+?>
   </body>
 </html>
