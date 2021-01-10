136e3dfa5f885009b8b1e3093240bffd9fad1455
Thomas <thomaslepretre78@gmail.com>
2021-01-10 15:02:54 +0100

    ajout charles mail 2

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 header("Content-type: text/plain");
 var_dump($_POST);
 $to = $_POST['envelope']['to'];
-if ($to == 'thomaslepretre78@gmail.com' || $to == ''){
+if ($to == 'thomaslepretre78@gmail.com' || $to == 'charlescretois@gmail.com'){
   http_response_code(201);
   echo("success\n");
 }else{
